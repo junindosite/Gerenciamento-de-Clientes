794c5b53cb86e4d9646b0a65c371d2526cdb2000
junindosite <juarezjunin2005@gmail.com>
2026-03-24 19:15:39 -0300

    Contr0ller

--- src/main/java/com/junior/crud/controller/ClienteController.java
+++ src/main/java/com/junior/crud/controller/ClienteController.java
@@ -1,4 +1,38 @@
 package com.junior.crud.controller;
 
+import com.junior.crud.entity.Cliente;
+import com.junior.crud.service.ClienteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    private final ClienteService service;
+
+    public ClienteController(ClienteService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Cliente salvar(@RequestBody Cliente cliente) {
+        return service.salvar(cliente);
+    }
+
+    @GetMapping
+    public List<Cliente> listar() {
+        return service.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente buscar(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
 }
