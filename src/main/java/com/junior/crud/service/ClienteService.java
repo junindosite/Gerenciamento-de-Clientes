5326946027fcd1509159aa4161ac5695aa6232e8
junindosite <juarezjunin2005@gmail.com>
2026-03-24 19:15:07 -0300

    Service

--- src/main/java/com/junior/crud/service/ClienteService.java
+++ src/main/java/com/junior/crud/service/ClienteService.java
@@ -1,4 +1,34 @@
 package com.junior.crud.service;
 
+import com.junior.crud.entity.Cliente;
+import com.junior.crud.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClienteService {
+
+    private final ClienteRepository repository;
+
+    public ClienteService(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    public Cliente salvar(Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    public List<Cliente> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Cliente buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
 }
